32d12936a5b393acb04d684e8215323bbd10bece
Simon <sburns77@gmail.com>
2015-06-15 19:35:36 -0500

    Form redirection after submission now works.

--- app.js
+++ app.js
@@ -5,83 +5,96 @@ angular.module('formApp', ['ngAnimate', 'ui.router'])
 
 // configuring our routes 
 // =============================================================================
-.config(function($stateProvider, $urlRouterProvider) {
-    
+.config(function($stateProvider, $urlRouterProvider,  $locationProvider) {
+    $locationProvider.html5Mode(true);
     $stateProvider
     
         // route to show our basic form (/form)
         .state('form', {
-            url: '/form',
+            url: '/',
             templateUrl: 'location.html',
             controller: 'formController'
         })
 
-        .state('form.createAccount', {
-            url: '/createAccount',
+        .state('createAccount', {
             templateUrl: 'create-account.html',
+            url: '/create-account',
             controller: 'formController'
         })
 
-         .state('form.care', {
+         .state('care', {
             url: '/care',
             templateUrl: 'care.html',
             controller: 'formController'
-        })
-        
+        })  
         //location page
-        .state('form.location', {
+        .state('location', {
             url: '/location',
             templateUrl: 'location.html',
             controller: 'formController'
         })
+
+         .state('comingSoon', {
+            url: '/comingSoon',
+            templateUrl: 'comingSoon.html',
+            controller: 'formController'
+        })
         
-        // url will be /form/payment
-        .state('form.payment', {
+        .state('payment', {
             url: '/payment',
             templateUrl: 'form-payment.html'
         });
-       
-
     // catch all route
     // send users to the form page 
-    $urlRouterProvider.otherwise('/form');
+    // $urlRouterProvider.otherwise('/form');
 
-
-})
-
-.controller('geoCtrl', function($scope, $http) {
-
-    $http.get("http://jeocoder.herokuapp.com/zips/" + formData.postalCode)
-    .success(function(response) {$scope.data = response;
-        console.log("logging response", response);
-    });
 })
 
 // our controller for the form
 // =============================================================================
-.controller('formController', function($scope, $http) {
-
-    // $http.get("http://jeocoder.herokuapp.com/zips/94110")
-    // .success(function(response) {$scope.data = response;
-    //     console.log("logging response", response);
-    //     console.log("logging response", response.city);
-    // });
-
+.controller('formController', function($scope, $http, $state, $rootScope, $location) {
     // we will store all of our form data in this object
     $scope.formData = {};
 
     $scope.fetch = function (userInput){
         $http.get("http://jeocoder.herokuapp.com/zips/" + userInput)
         .success(function(response) {
-            $scope.data = response;
+            $scope.formData.data = response.city;
             console.log("logging response", response);
+            if ($scope.formData.data === 'NEW YORK'){
+                // $state.go('form.createAccount');
+
+                $location.path('/create-account');
+                console.log('locat', $location.path());
+                console.log('NYYYY');
+                $scope.formData.next = 'form.createAccount';
+                console.log($scope.formData.next);
+            } else {
+                // $state.go('form.comingSoon');
+
+                $location.path('/createAccount');
+                console.log('loc', $location.path());
+                console.log('logging comming soon');
+                $scope.next = 'form.comingSoon';
+                console.log($scope.next);
+             }
         });
     }
-    
-    // function to process the form
+
+
+//     app.run(['$state', '$rootScope', '$timeout', function ($state, $rootScope, $timeout) {
+//   var appStarted = 0; // flag to redirect only once when app is started
+//   $rootScope.$on('$stateChangeStart', 
+//   function(event, toState, toParams, fromState, fromParams) { 
+//     if(appStarted) return;
+//     appStarted = 1;   
+//     event.preventDefault(); //prevents from resolving requested url
+//     $state.go('home.other'); //redirects to 'home.other' state url
+//   });  
+// }]);
+     // function to process the form
     $scope.processForm = function() {
         alert('awesome!');  
     };
-   
 });
 
